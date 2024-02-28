2a6f66e6e69a7c06d21434fbc8c35a3f5e924c9d
Vladimir Kurilin <vladimirkurilin@gmail.com>
2024-02-28 23:33:12 +0300

    wip: copy paste from ui

--- packages/avalon-app/src/main.ts
+++ packages/avalon-app/src/main.ts
@@ -1,5 +1,37 @@
-import { createApp } from 'vue';
-import './styles.css';
-import App from './App.vue';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
-createApp(App).mount('#app');
+import { createApp } from 'vue';
+import App from './App.vue';
+// копипаста с ui/main.ts, поменял @/ на @avalon/ui/src/
+import router from '@avalon/ui/src/router';
+import { store, key } from '@avalon/ui/src/store';
+
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import { aliases, md } from 'vuetify/iconsets/md';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'light',
+  },
+  defaults: {
+    VBtn: {
+      color: 'primary',
+      variants: 'tonal',
+      rounded: 'lg',
+    },
+  },
+  icons: {
+    defaultSet: 'md',
+    aliases,
+    sets: {
+      md,
+    },
+  },
+});
+
+createApp(App).use(store, key).use(router).use(vuetify).mount('#app');
